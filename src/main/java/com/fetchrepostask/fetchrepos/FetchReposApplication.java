cda8675303970c8d127689bef788dfb8aff0895c
NLenwa <34138716+NLenwa@users.noreply.github.com>
2023-06-05 01:54:41 +0200

    Spring initializr

---
+++ src/main/java/com/fetchrepostask/fetchrepos/FetchReposApplication.java
@@ -0,0 +1,17 @@
+package com.fetchrepostask.fetchrepos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class FetchReposApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(FetchReposApplication.class, args);
+	}
+
+
+}
---
+++ src/test/java/com/fetchrepostask/fetchrepos/FetchreposApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fetchrepostask.fetchrepos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FetchreposApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
